e7c3d9e6dab674b7bce6907d42185c1f5b4d084f
Duarte2018 <duarte.pro2018@gmail.com>
2019-10-02 12:47:55 +0100

    Create german greeting and default profile

--- src/main/java/guru/springframework/didemo/services/PrimaryGreetingGerman.java
+++ src/main/java/guru/springframework/didemo/services/PrimaryGreetingGerman.java
@@ -6,11 +6,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile({"en","default"})
-public class PrimaryGreetingService implements GreetingService {
-
+@Profile("de")
+public class PrimaryGreetingGerman implements GreetingService{
     @Override
     public String sayGreeting() {
-        return "Hello from primary Service";
+        return "Primarer Grussdienst";
     }
 }
